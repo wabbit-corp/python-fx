b059515663e9a0e055c7b59b37bb6800f6823f3f
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2015-11-12 10:57:35 -0500

    First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fx',
+      version='0.1',
+      description='functional generator patterns',
+      author='Alexander Konovalov',
+      author_email='alex.knvl@gmail.com',
+      packages=['fx'])
