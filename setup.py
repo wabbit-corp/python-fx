aa27c9a469cda15322af05b88bbb21a6e285aec2
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2017-11-05 20:53:46 -0500

    Module name change.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from distutils.core import setup
 
-setup(name='fx',
-      version='0.1',
+setup(name='osunlp-fx',
+      version='0.2',
       description='functional generator patterns',
       author='Alexander Konovalov',
       author_email='alex.knvl@gmail.com',
